0bc964f75d2eb8c9a6a6508741d7234c32af2195
Ankit <contactankit55@gmail.com>
2017-06-19 19:59:49 +0530

    defining angularjs application and controller

--- myCalculator/assets/calculator.js
+++ myCalculator/assets/calculator.js
@@ -1,5 +1,23 @@
 /* JavaScript Document */
 
-/* Script goes here */
+/* everything that we are going to add here will be
+the angular javascript content of the html */
 
+(function() {
 
+    /* all modules that should be available in an
+    application should be declared this way */
+    var app = angular.module('myCalculator', []);
+
+    /* scope service allows us to pass variables between html and javascript */
+    app.controller('CalculatorController', ['$scope', function($scope) {
+        $scope.lumen_options = [375, 600, 900, 1125, 1600];
+        $scope.current_lumens = 600;
+				$scope.current_cost = 13;
+				$scope.current_hours = 4;
+				$scope.total_days = 365;
+    }]);
+
+    /* these are the two main parts of our application */
+
+})();
