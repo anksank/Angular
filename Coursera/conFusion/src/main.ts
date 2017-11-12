572a275b0f8234c41d3f127d78c9f0ca94e60e55
Ankit <contactankit55@gmail.com>
2017-11-12 14:20:06 +0530

    Going through Components and Modules

--- Coursera/conFusion/src/main.ts
+++ Coursera/conFusion/src/main.ts
@@ -1,12 +1,18 @@
+// import this module from angular core library
 import { enableProdMode } from '@angular/core';
+
+// import following module from platform browser dynamic module
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+// importing from the file system
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
-  enableProdMode();
+  	enableProdMode();
 }
 
+// platformBrowserDynamic enables to bootstrap an angular application
+// by taking the root module as parameter 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+  	.catch(err => console.log(err));
