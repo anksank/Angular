f14c3dbd808d759b5ed3bd6c963e7d465a3f833c
Ankit <contactankit55@gmail.com>
2017-11-12 15:23:33 +0530

    Angular Configurations

--- Coursera/conFusion/src/app/app.component.ts
+++ Coursera/conFusion/src/app/app.component.ts
@@ -1,5 +1,12 @@
+// this file supports the root component that
+// forms part of our angular application
 import { Component } from '@angular/core';
 
+// Below is a component declarator 
+// It takes a javascript object as a parameter
+// selector: app-root (This is why the index.html references this tag)
+// templateUrl: points to html file that contains the template correspnding to this Component
+// styleUrl: points to the scss code that can be used for css styling for component module
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
--- Coursera/conFusion/src/app/app.module.ts
+++ Coursera/conFusion/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
 		AppComponent
 	],
 	imports: [
-		BrowserModule
+		BrowserModule,
+		BrowserAnimationsModule,
+		MaterialModule,
+		FlexLayoutModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
